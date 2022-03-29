5bec35df4d2ab248fe65da417bea5b7d3d412089
taufanbasri <taufanprasetyobasri@gmail.com>
2022-03-29 22:01:03 +0700

    Setup Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pale-blue": "hsl(225, 100%, 94%)",
+        "bright-blue": "hsl(245, 75%, 52%)",
+        "very-pale-blue": "hsl(225, 100%, 98%)",
+        "desaturated-blue": "hsl(224, 23%, 55%)",
+        "dark-blue": "hsl(223, 47%, 23%)"
+      },
+      fontFamily: {
+        "red-hat": ['Red Hat Display', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
